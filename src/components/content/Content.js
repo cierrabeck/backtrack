b6e1476cc1be9c0f910e68efe33b7af0e86de145
cierrabeck <cnb44@cornell.edu>
2020-08-31 14:41:57 -0400

    updated styling

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -24,17 +24,17 @@ const Content = props => {
     if (originalityScore<10) {
       return ("congratulations, you have the most basic music taste")
     } else if (originalityScore<20) {
-      return ("something less than 20")
+      return ("while you have some obscure tracks in your mix, most of your music is mainstream")
     } else if (originalityScore<30) {
-      return ("something less than 30")
+      return ("while you have some obscure tracks in your mix, most of your music is mainstream")
     } else if (originalityScore<40) {
-      return ("something less than 40")
+      return ("while you have some obscure tracks in your mix, most of your music is mainstream")
     } else if (originalityScore<50) {
-      return ("something less than 50")
+      return ("while you have some obscure tracks in your mix, most of your music is mainstream")
     } else if (originalityScore<70) {
-      return ("something less than 70")
+      return ("while you have some obscure tracks in your mix, most of your music is mainstream")
     } else if (originalityScore<90) {
-      return ("something less than 90")
+      return ("while you have some obscure tracks in your mix, most of your music is mainstream")
     } else {
       return ("you only listen to SUPER obscure music. where do you even find these?")
     }
@@ -65,12 +65,30 @@ const Content = props => {
 
   return (
     <div className="App">
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet"></link>
       <div className="main">
         <h3> your month in review </h3>
-        <h2> originality score: {originalityScore} </h2>
-        <h2> {mapOriginality()} </h2>
-        <h2> most obscure song: {mostObscure.name} by {mostObscure.artists[0].name}</h2>
-        <h2> most basic song: {mostBasic.name} by {mostBasic.artists[0].name} </h2>
+        <div className="originalityScore">
+          <h2 className="originalityh2"> originality score </h2>
+          <p className="disc"/>
+          <div className="plaque">
+            <h4 className="score"> {originalityScore} </h4>
+            <h2 className="scoreDescription"> {mapOriginality()} </h2>
+          </div>
+        </div>
+        <div className="plaques">
+          <div className="most"> 
+            <h2 className="originalityh2"> most obscure song </h2>
+            <img src={mostObscure.album.images[0].url} className="cover"/>
+            <h2 className="songTitle"> {mostObscure.name} by {mostObscure.artists[0].name} </h2>
+          </div>
+          <div className="most"> 
+            <h2 className="originalityh2"> most basic song </h2>
+            <img src={mostBasic.album.images[0].url} className="cover"/>
+            <h2 className="songTitle"> {mostBasic.name} by {mostBasic.artists[0].name} </h2>
+          </div>
+        </div>
+
         <div className="top-songs">
           <div className="covers">
             <img src={props.items[4].album.images[0].url} className="album-cover"/>
@@ -78,9 +96,9 @@ const Content = props => {
             <img src={props.items[2].album.images[0].url} className="album-cover2"/>
             <img src={props.items[1].album.images[0].url} className="album-cover3"/>
             <img src={props.items[0].album.images[0].url} className="album-cover4"/>
-
           </div>
           <div className="songs"> 
+          <h1>TOP 5 TRACKS: </h1> 
             <ol>
               <li>{props.items[0].name} by {props.items[0].artists[0].name}</li>
               <li>{props.items[1].name} by {props.items[1].artists[0].name}</li>
@@ -89,9 +107,7 @@ const Content = props => {
               <li>{props.items[4].name} by {props.items[4].artists[0].name}</li>
             </ol>
           </div>
-
         </div>
-        {/* <div className="background" style={backgroundStyles} />{" "} */}
       </div>
     </div>
   );
